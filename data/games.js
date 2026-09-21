a8d1cada95df82113209dfbfdcf322a615c24756
Madison <madisoncanejo@gmail.com>
2026-09-20 22:27:54 -0300

    Adiciona game Defensor Estelar (1ª Série)

--- data/games.js
+++ data/games.js
@@ -17,5 +17,12 @@ var GAMES = [
     descricao: "Game estilo arcade sobre a 1ª Lei da Termodinâmica (processos isotérmico, isocórico, isobárico e adiabático). Assunto da 2ª Série do Ensino Médio.",
     url: "/games-html/termo1-arcade.html",
     data: "2026-09-17"
+  },
+  {
+    tema: "Astronomia",
+    titulo: "Defensor Estelar",
+    descricao: "Game baseado na aula \"Somos Poeira das Estrelas\", sobre a origem dos elementos químicos. Assunto da 1ª Série do Ensino Médio.",
+    url: "/games-html/defensor-estelar.html",
+    data: "2026-09-20"
   }
 ];
